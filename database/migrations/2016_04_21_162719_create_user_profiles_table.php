1754c47477bd25332aa3acc40fe1ebea1f87438b
Jyrone Parker <jyrone.parker@gmail.com>
2016-04-24 17:59:42 +0000

    Getting views together

--- database/migrations/2016_04_21_162719_create_user_profiles_table.php
+++ database/migrations/2016_04_21_162719_create_user_profiles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDriverProfilesTable extends Migration
+class CreateUserProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,12 @@ class CreateDriverProfilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('driver_profiles', function (Blueprint $table) {
+        Schema::create('user_profiles', function (Blueprint $table) {
              $table->increments('id');
 	           $table->integer('user_id')->unsigned();
 	           $table->foreign('user_id')->references('id')->on('users');
 	           $table->string('address');
+             $table->string('phone');
 	           $table->boolean('verified')->default(false);
              $table->timestamps();
         });
